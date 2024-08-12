277e94c7d46fa7accadba2c972c23a92f3e399b0
hanachiru <hayato.sato@dena.com>
2024-08-13 02:12:38 +0900

    Benchmark追加

---
+++ RapidEnum.Benchmark/Program.cs
@@ -0,0 +1,86 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FastEnumUtility;
+using RapidEnum;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<BenchmarkTarget>();
+    }
+}
+
+[RapidEnum]
+public enum Target
+{
+    A,
+    B,
+    C,
+    D
+}
+
+[MemoryDiagnoser]
+//[SimpleJob(RunStrategy.ColdStart)]
+public class BenchmarkTarget
+{
+    [Benchmark]
+    public void RapidEnum_GetValues()
+    {
+        _ = TargetEnumExtensions.GetValues();
+    }
+
+    [Benchmark]
+    public void FastEnum_GetValues()
+    {
+        _ = FastEnum.GetValues<Target>();
+    }
+
+    [Benchmark]
+    public void RapidEnum_GetNames()
+    {
+        _ = TargetEnumExtensions.GetNames();
+    }
+
+    [Benchmark]
+    public void FastEnum_GetNames()
+    {
+        _ = FastEnum.GetNames<Target>();
+    }
+
+    [Benchmark]
+    public void RapidEnum_ToString()
+    {
+        _ = Target.A.ToStringFast();
+    }
+
+    [Benchmark]
+    public void FastEnum_ToString()
+    {
+        _ = Target.A.FastToString();
+    }
+
+    [Benchmark]
+    public void RapidEnum_IsDefines()
+    {
+        _ = TargetEnumExtensions.IsDefined("2");
+    }
+
+    [Benchmark]
+    public void FastEnum_IsDefines()
+    {
+        _ = FastEnum.IsDefined<Target>("2");
+    }
+
+    [Benchmark]
+    public void RapidEnum_TryParse()
+    {
+        _ = TargetEnumExtensions.TryParse("A", out var _);
+    }
+
+    [Benchmark]
+    public void FastEnum_TryParse()
+    {
+        _ = FastEnum.TryParse<Target>("A", out var _);
+    }
+}
